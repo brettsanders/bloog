c664169e7532b4451212cf464a377076c2b03c26
Brett Sanders <brettrsanders@gmail.com>
2013-01-09 15:04:48 -0800

    test Blog#new_post, ISOLATED FROM POST MODEL

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,15 @@
 class Blog
   attr_reader :entries
+  attr_writer :post_source
 
   def initialize
     @entries = []
+  end
+
+  def new_post
+    post_source.call.tap do |p|
+      p.blog = self
+    end
   end
 
   def title
@@ -11,4 +18,14 @@ class Blog
   def subtitle
     "The trusted source for drying paint news & opinion"
   end
+
+  private
+  def post_source
+    @post_source ||= Post.public_method(:new)
+  end
 end
+
+# public_method instantiates a call-able Method object
+# during normal operation, Blog will use this method reference
+# (the equivalent of calling Post.new)
+# But, we can subtitute any call-able object when testing the class
