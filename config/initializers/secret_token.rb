a355ab24d84fcacf7d586e53f364e56f13352aa9
Brett Sanders <brettrsanders@gmail.com>
2013-01-09 10:05:32 -0800

    First commit, rails new bloog without test unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloog::Application.config.secret_token = 'd27d243e5e17d621d5673e72e238ac11fd2b1b2006c21a3c74353b201630a1e5471658a46ddc713b2ab419db9ad80079b85f9b1616306af26e5207ead0198e99'
